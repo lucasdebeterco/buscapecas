946c85551ad51772d4b6722af83a0f2958055e75
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-03-26 23:17:40 -0300

    starting project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# buscapecas
-buscapecas
+# Buscapecas
+Trabalho de conclusão de curso em bacharelado em Sistemas de informação - Lucas Debeterco
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+      <>
+        <Header />
+        Content
+        <Footer />
+      </>
+  )
+}
+
+export default App
