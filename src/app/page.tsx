b54f34e185522aa0c381307bb600d6b6c195ce7c
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-01 23:27:29 -0300

    migrating project to nextJs

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+            <header className='bg-red-500'>
+                Header
+            </header>
+
+            <footer>
+                footer
+            </footer>
+        </>
+    )
+}
