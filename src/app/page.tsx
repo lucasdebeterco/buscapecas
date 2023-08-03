576e27b16ff891a4cb69ad5abd224d9d8286545f
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-02 23:58:44 -0300

    starting layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,26 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <>
-            <header className='bg-red-500'>
-                Header
+        <div className='h-screen'>
+            <header className='flex justify-center bg-red-500 py-2'>
+                <h1 className='text-white font-bold'>BUSCAPECA</h1>
             </header>
 
-            <footer>
-                footer
+            <form>
+                <label htmlFor='pesquisar'></label>
+                <input type='text' name='pesquisar' placeholder='Pesquisar item..'/>
+
+                <label htmlFor='submit'></label>
+                <input name='submit' type='submit'/>
+            </form>
+
+            <div className='productList'>
+                Área de listagem de produtos
+            </div>
+
+            <footer className='flex justify-center bg-red-500 py-8'>
+                <h1 className='text-white font-bold'>Trabalho de Conclusão de Curso - 2023</h1>
             </footer>
-        </>
+        </div>
     )
 }
