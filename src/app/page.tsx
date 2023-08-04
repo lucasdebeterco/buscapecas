b5abddc7c6ade7650eb54d91f86a89613dded1cc
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-03 23:55:12 -0300

    Including fixed data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,52 @@
 import Image from 'next/image'
 
+interface ProductProps {
+    nome: string
+}
+
+// Buscar aprender sobre conceito de crawlers e web scraping
+
 export default function Home() {
+    const productList = [
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'},
+        {'nome': 'teste'}
+    ]
+
     return (
         <div className='h-screen'>
-            <header className='flex justify-center bg-red-500 py-2'>
-                <h1 className='text-white font-bold'>BUSCAPECA</h1>
+            <header className='flex justify-center bg-red-500 py-6'>
+                <h1 className='text-white font-bold text-3xl'>BUSCAPECA</h1>
             </header>
 
-            <form>
+            <form className='h-20 mt-3'>
                 <label htmlFor='pesquisar'></label>
                 <input type='text' name='pesquisar' placeholder='Pesquisar item..'/>
 
                 <label htmlFor='submit'></label>
-                <input name='submit' type='submit'/>
+                <input className='py-2 px-4 bg-red-500 text-white rounded-[8px] hover:bg-red-600 transition' name='submit' type='submit'/>
             </form>
 
-            <div className='productList'>
-                Área de listagem de produtos
+            <div className='productList flex gap-8 flex-wrap p-4'>
+                {productList.map((product:ProductProps) => {
+                    return (
+                        <div className='h-72 w-64 border-2 p-4 rounded-2xl'>
+                            <span>{product.nome}</span>
+                        </div>
+                    )
+                })}
             </div>
 
             <footer className='flex justify-center bg-red-500 py-8'>
