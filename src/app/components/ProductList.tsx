7110ce4c99edeb1c14d904024552bb9f0bfca98f
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-11 23:47:16 -0300

    product cards and input styling

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -4,6 +4,7 @@ import {v4 as uuidv4} from "uuid";
 import {useState} from "react";
 import axios from "axios";
 import {IProduct} from "@/app/types/Product.types";
+import {MagnifyingGlass} from "@phosphor-icons/react";
 
 export function ProductList() {
     let [productList, setProductList]  = useState([])
@@ -23,15 +24,17 @@ export function ProductList() {
     }
 
     return (
-        <div className='max-w-[1240px] my-0 mx-[auto]'>
-            <form onSubmit={getProducts} className='mt-8' action='#'>
+        <div className='max-w-[1240px] px-4 my-0 mx-[auto]'>
+            <form onSubmit={getProducts} className='flex column items-center mt-8' action='#'>
                 <label htmlFor='searchItem'></label>
                 <input type='text'
                        name='searchItem'
                        placeholder='Pesquisar item..'
-                       className='outline-0 border-[1px] py-2 px-4 rounded-[8px]'
+                       className='outline-0 border-[1px] h-[36px] py-1 px-4 rounded-[8px]'
                 />
-                <input className='py-2 px-4 ml-4 bg-red-500 text-white rounded-[8px] hover:bg-red-600 transition' name='submit' type='submit'/>
+                <button className='h-[36px] px-3 ml-2 bg-red-500 text-white rounded-[8px] hover:bg-red-600 cursor-pointer transition' name='submit' type='submit'>
+                    <MagnifyingGlass color='#fff' size={20} />
+                </button>
             </form>
 
             { isLoading ? (
@@ -40,10 +43,10 @@ export function ProductList() {
                 <div className='productList grid grid-cols-5 gap-6 my-8'>
                     {productList.map((product:IProduct) => {
                         return (
-                            <div key={uuidv4()} className='flex flex-col justify-between border-[1px] p-4 rounded-2xl'>
+                            <div key={uuidv4()} className='flex flex-col justify-between border-[2px] hover:border-red-600 p-4 rounded-2xl'>
                                 <>
-                                    <div className='flex justify-center items-center h-[180px] w-[180px]'>
-                                        <img className='' src={product.image} />
+                                    <div className='flex justify-center items-center'>
+                                        <img className='w-full h-auto' src={product.image} />
                                     </div>
                                     <span className='text-[0.825rem] font-bold h-[58px] overflow-hidden'>{product.title}</span>
                                     <span className='text-[0.825rem] font-bold text-red-600 mt-2'>{product.price}</span>
@@ -51,7 +54,7 @@ export function ProductList() {
                                 <a
                                     href={product.link}
                                     target='_blank'
-                                    className='text-center text-white bg-red-600 mt-6 rounded-[6px] py-[6px] font-bold text-[0.75rem]'
+                                    className='text-center text-white bg-red-600 hover:bg-red-700 transition mt-6 rounded-[6px] py-[6px] font-bold text-[0.75rem]'
                                 >
                                     Ver produto na Loja
                                 </a>
@@ -61,7 +64,6 @@ export function ProductList() {
                 </div>
             ) : (
                 <strong>Nenhum resultado encontrado</strong>
-
             )}
         </div>
     )
