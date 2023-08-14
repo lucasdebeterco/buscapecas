cd61ded8318d12b32dc34ea17b8c3d103a8988a1
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-13 23:59:00 -0300

    trying to get pichau products

--- src/pages/api/scrapper.ts
+++ src/pages/api/scrapper.ts
@@ -5,22 +5,35 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import {IProduct} from "@/app/types/Product.types";
 
 export default async function scrapper(req: NextApiRequest, res: NextApiResponse) {
-    const testUrl = 'https://www.kabum.com.br/busca/'
+    const kabumUrl = 'https://www.kabum.com.br/busca/'
+    const pichauUrl = 'https://www.pichau.com.br/search?q='
+
     const method  = req.method
     const searchItem = req.query.searchItem && req.query.searchItem.toString()
 
     if(method === 'GET') {
+        const products: IProduct[] = []
+
+        //await getProducts(products, kabumUrl, '.productCard')
+        await getProducts(products, pichauUrl, '.MuiGrid-item')
+
+        //products.sort((a, b) => (parseFloat(a.price) > parseFloat(b.price)) ? 1: -1);
+        res.send(products)
+    } else {
+        res.send('Method not allowed')
+    }
+
+    async function getProducts(products: IProduct[], searchUrl: string, selector: string) {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
 
-        await page.goto(testUrl + slugify(searchItem ? searchItem : ''))
-
+        await page.goto(searchUrl + slugify(searchItem ? searchItem : ''))
         const html = await page.content()
         const $ = load(html)
 
-        const products: IProduct[] = []
+        console.log(html)
 
-        $('.productCard').each((i, el) => {
+        $(selector).each((i, el) => {
             const image = $('.imageCard', el).attr('src')
             const title = $('.nameCard', el).text()
             const price = $('.priceCard', el).text()
@@ -29,11 +42,8 @@ export default async function scrapper(req: NextApiRequest, res: NextApiResponse
             products.push({
                 lojaId: 1, image, title, price, link
             })
-        })
 
-        //products.sort((a, b) => (parseFloat(a.price) > parseFloat(b.price)) ? 1: -1);
-        res.send(products)
-    } else {
-        res.send('Method not allowed')
+            console.log(products)
+        })
     }
-}
+ }
