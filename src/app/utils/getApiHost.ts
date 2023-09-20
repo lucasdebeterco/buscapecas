64487e57a49a00f819734de5eadc68e1d998c650
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-09-19 23:39:59 -0300

    link to aws ec2 api instance

--- src/app/utils/getApiHost.ts
+++ src/app/utils/getApiHost.ts
@@ -1,3 +1,3 @@
 export function getApiHost() {
-    return window.location.host.includes('localhost') ? 'http://localhost:3000/' : 'https://buscapeca-api.onrender.com/'
+    return window.location.host.includes('localhost') ? 'http://localhost:3000/' : 'http://15.229.69.25:3000/'
 }
