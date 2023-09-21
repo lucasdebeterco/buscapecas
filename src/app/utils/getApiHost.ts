3565cb17ac4417c06a6cd9200a775a8801497c11
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-09-20 22:56:08 -0300

    update aws ec2 public ip

--- src/app/utils/getApiHost.ts
+++ src/app/utils/getApiHost.ts
@@ -1,3 +1,3 @@
 export function getApiHost() {
-    return window.location.host.includes('localhost') ? 'http://localhost:3000/' : 'http://15.229.69.25:3000/'
+    return window.location.host.includes('localhost') ? 'http://localhost:3000/' : 'http://15.228.15.38:3000/'
 }
