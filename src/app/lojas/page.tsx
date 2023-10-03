bf171b20527561870207fdc20b3aa2bddb6a15fa
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-10-02 23:13:50 -0300

    mapping lojas from api

--- src/app/lojas/page.tsx
+++ src/app/lojas/page.tsx
@@ -2,16 +2,46 @@
 
 import {Header} from "@/app/components/Header";
 import {Footer} from "@/app/components/Footer";
-import {ProductList} from "@/app/components/ProductList";
+import { useEffect, useState } from 'react';
+import { getApiHost } from '@/app/utils/getApiHost';
+import axios from 'axios';
+import { Loader } from '@/app/components/Loader/Loader';
 
 export default function Home() {
+    let [lojas, setLojas]  = useState([])
+    let [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        axios.get(`${getApiHost()}lojas`)
+            .then((response: any) => {
+                setLojas(response.data)
+            }).catch((error: any) => {
+            console.error("Error:", error);
+        }).finally(() => {
+            setIsLoading(false)
+        });
+    }, [])
+
     return (
-        <div>
-            <Header />
-            <div className='max-w-[1240px] px-4 my-0 mx-[auto] min-h-[90vh]'>
-                <h1>Lojas</h1>
-            </div>
-            <Footer />
-        </div>
+        <>
+            { isLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    <Header />
+                    <div className='max-w-[1240px] px-4 my-0 mx-[auto] min-h-[90vh]'>
+                        <h1>Lojas</h1>
+
+                        {lojas.map((loja: any) => (
+                            <div key={loja.id}>
+                                Nome: {loja.nome}
+                                Likes: {loja.likes}
+                            </div>
+                        ))}
+                    </div>
+                    <Footer />
+                </div>
+            )}
+        </>
     )
 }
